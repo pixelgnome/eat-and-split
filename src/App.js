7b69c0521282d36951a18c8cd99fe9be7f70bcd8
Salvatore Filippone <sfilippone@gmail.com>
2026-04-16 16:29:34 -0700

    finished ui. needs functionality

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -56,7 +59,43 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are square</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🙋🏻‍♂️ Friend Name</label>
+      <input type="text" />
+      <label>📸 Image Url</label>
+      <input type="text" />
+      <Button>Select</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X:</h2>
+      <label>💵 Bill value:</label>
+      <input type="text" />
+      <label>🧍🏻 Your expense:</label>
+      <input type="text" />
+      <label>👬 X's expense:</label>
+      <input type="text" disabled />
+      <label>Who is paying the bill?</label>
+      <select>
+        <option value="you">You</option>
+        <option value="friend">Friend</option>
+      </select>
+      <Button>Split Bill</Button>
+    </form>
   );
 }
